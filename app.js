cc1ab659b0d987ed81437f7e61f3b678ec7f1404
Quimey <quimeymata@gmail.com>
2022-09-30 01:11:43 -0300

    Adds api.mapbox.com as a supported source

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const app = express();
 
 // Connect to the database
 const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/surf-shop';
-mongoose.connect(dbUrl);
+mongoose.connect('mongodb://127.0.0.1:27017/surf-shop');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'Connection error:'));
@@ -105,6 +105,25 @@ app.use(function(req, res, next) {
   
   next();
 })
+
+// Add api.mapbox.com as a supported source in your project.
+const CSP = 'Content-Security-Policy';
+const POLICY =
+  "default-src 'self' https://*.mapbox.com ;" +
+  "base-uri 'self';block-all-mixed-content;" +
+  "font-src 'self' https: data:;" +
+  "frame-ancestors 'self';" +
+  "img-src http://localhost:8000 'self' blob: data:;" +
+  "object-src 'none';" +
+  "script-src https: cdn.jsdelivr.net cdnjs.cloudflare.com api.mapbox.com 'self' blob: ;" +
+  "script-src-attr 'none';" +
+  "style-src 'self' https: 'unsafe-inline';" +
+  'upgrade-insecure-requests;';
+
+app.use((req, res, next) => {
+    res.setHeader(CSP, POLICY);
+    next();
+  });
 
 //Mount Routes
 app.use('/', indexRouter);
